1d7fa7fae3aa6a93d88e330aea03168a832a8bc2
hoffmanick <134728053+hoffmanick@users.noreply.github.com>
2024-03-06 15:05:30 -0800

    placeholders
    for components in database landing pages

---
+++ src/views/resources/databaseelements/databasedetails.vue
@@ -0,0 +1,63 @@
+<script setup>
+  import Panel from 'primevue/panel';
+  import Card from 'primevue/card';
+  import ChronDetails from "@/views/resources/datasetelements/chronologydetails.vue"
+  import TaxaDetails from "@/views/resources/datasetelements/taxaLoad.vue"
+  import PublicationsDetails from "@/views/resources/datasetelements/publicationdetails.vue"
+  import OtherDetails from "@/views/resources/datasetelements/otherdatasetdetails.vue"
+  const props = defineProps(['title'])
+  let dstype = props.title.site.datasets[0].datasettype[0].toUpperCase() +
+    props.title.site.datasets[0].datasettype.slice(1)
+
+</script>
+
+<template>
+    <Panel toggleable>
+        <template #header>
+            <h2>{{ dstype }} Dataset Details</h2>
+        </template>
+        <div v-if="props.title.site">
+            <strong>Constituent Database: </strong>
+            <span v-if="props.title.site.datasets[0].database" class="m-0">
+                {{ props.title.site.datasets[0].database }}
+            </span>
+            <span v-else>None Reported</span>
+            <br><strong>Dataset Notes: </strong>
+            <span v-if="props.title.site.datasets[0].datasetnotes">
+                <em>{{props.title.site.datasets[0].datasetnotes}}</em>
+            </span>
+            <span v-else>None Reported</span>
+            <h3>Dataset PIs</h3>
+            <div v-if="props.title.site.datasets[0].datasetpi">
+                <div class="grid">
+                    <div class="flex flex-row flex-wrap">
+                        <div v-for="name in props.title.site.datasets[0].datasetpi">
+                            <Card>
+                                <template #content>
+                                    {{ name.contactname }}
+                                </template>
+                            </Card>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div v-else>None Reported</div>
+        </div>
+        <TaxaDetails />
+    </Panel>
+
+    <ChronDetails :datasetid="props.title.site.datasets[0].datasetid" />
+    <PublicationsDetails :datasetid="props.title.site.datasets[0].datasetid" />
+    <OtherDetails :siteid="props.title.site.siteid" />
+</template>
+
+<script>
+  export default {
+    name: 'DatasetDetails',
+    data () {
+      return {
+        msg: 'Mapbox element has rendered.'
+      }
+    }
+  }
+</script>
---
+++ src/views/resources/databaseelements/sitedetails.vue
@@ -0,0 +1,51 @@
+<script setup>
+import SiteMapAll from '@/views/resources/mapsite_all.vue'
+import Panel from 'primevue/panel'
+    const props = defineProps(['title'])
+
+</script>
+
+<template>
+    <Panel>
+        <template #header>
+            <h2>{{props.title[0].site.datasets[0].database}} Details</h2>
+        </template>
+        <p>{{props.title[0].site.datasets[0].database}}</p>
+  
+        <div class="col" style="max-height:200px">
+            <SiteMapAll :location="props.title[0].site.geography" />
+        </div>
+        <div v-if="props.title.site">
+
+            <div class="grid">
+                <div class="col-8">
+                    <strong>Site Name:</strong> <br />
+                    <strong>Description: </strong>
+                    <span v-if="props.title.site.sitedescription">
+                        <em>hello </em>
+
+                    </span>
+                    <span v-else> None</span>
+                    <br />
+                    <strong>Notes: </strong>
+                    <span v-if="props.title.site.sitenotes">
+                        <em> howdy</em>
+                    </span>
+                    <span v-else> None</span>
+                </div>
+
+            </div>
+        </div>
+    </Panel>
+</template>
+
+<script>
+export default {
+  name: 'SiteDetails',
+  data() {
+    return {
+      msg: 'Mapbox element has rendered.'
+    }
+  }
+}
+</script>
