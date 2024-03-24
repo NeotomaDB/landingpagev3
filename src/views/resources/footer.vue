b65430d776ba08818475a83fb5bbdf8ab1e57d1f
hoffmanick <134728053+hoffmanick@users.noreply.github.com>
2024-03-24 06:40:19 -0700

    modified footer
    got rid of extra card, changed grid layout and alignment a bit to make it symmetric

--- src/views/resources/footer.vue
+++ src/views/resources/footer.vue
@@ -5,26 +5,30 @@ import Avatar from 'primevue/avatar';
 <template>
   <Card>
     <template #content>
-      <div class="grid">
-        <div class="col-1" style="text-align:left;">
+      <div class="grid" style="justify-content:space-between;">
+        <div class="col-5 flex" style="justify-content:space-evenly;">
+        <div style="text-align:left;">
           <img src="@/assets/nsf.png" width="100" height="100" alt="National Science Foundation logo">
         </div>
-        <div class="col-3 flex">
-          <Card class="align-middle" style="text-align:left;padding: left 5px;">
-            <template #content>
-              <Avatar image="../nsf.png" />
+        <div >
+        <!--  <Card class="align-middle" style="text-align:left;padding: left 5px;"> 
+            <template #content> 
+              <Avatar image="../nsf.png" /> -->
             <br><a href=https://www.nsf.gov/>National Science Foundation</a>
             <br>Directorate for Geosciences
             <br>Division of Earth Sciences
-            </template>
-          </Card>
+         <!--    </template>
+         </Card> -->
         </div>
-        <div class="col-6" style="text-align:right;">
-          <a href=https://earthcube.org>EarthCube</a><br>Integrative Activities<br>National Sciences Foundation
+      </div>
+      <div class="col-5 flex" style="justify-content:space-evenly;align-items:center;">
+        <div style="text-align:right;">
+          <a href=https://earthcube.org>EarthCube</a><br>Integrative Activities<br>National Science Foundation
         </div>
-        <div class="col-2" style="text-align:left;">
+        <div style="text-align:left;">
           <img src="@/assets/logo_earthcube_cube-only_SMALL.png" style="margin-top:10px" width=100 height=100 alt="EarthCube logo.">
         </div>
+      </div>
       </div>
     </template>
   </Card>
