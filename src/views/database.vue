41c885f52bd87249cb449d2b1a56d120110ebeb1
hoffmanick <134728053+hoffmanick@users.noreply.github.com>
2024-04-02 12:05:52 -0700

    removed tabview
    removed tabview for spatial and temporal extent

--- src/views/database.vue
+++ src/views/database.vue
@@ -39,113 +39,11 @@
   const neotomaapi = import.meta.env.VITE_APP_API_ENDPOINT ?? 'https://api.neotomadb.org'
 
 
-  function loadconstDB() {
-    if (route.params.databaseid) {
-      console.log(route.params.databaseid)
-    return  fetch(neotomaapi + "/v2.0/apps/constdb/datasets?dbid=" + route.params.databaseid,
-      { method: "GET", headers: {'content-type': 'application/json'}})
-    .then(res1 => {
-      return res1.json()})
-    .then(json1 => {
-      constDBinfo.value = json1.data
-      
-   
-      uniqueDBsites.value = new Set();
-      constDBinfo.value.forEach(obj => uniqueDBsites.value.add(obj['siteid']));
-      uniqueDBsites.value = uniqueDBsites.value.size
-
-      uniqueDBsets.value = new Set();
-      constDBinfo.value.forEach(obj => uniqueDBsets.value.add(obj['datasetid']));
-      uniqueDBsets.value = uniqueDBsets.value.size
-      datasettypes.value = constDBinfo.value.reduce((acc, obj) => {
-          const type = obj.datasettype;
-          acc[type] = (acc[type] || 0) + 1;
-          return acc;}, {});
-    //  console.log(datasettypes.value)
-
-  datasettypes.value = Object.entries(datasettypes.value).map(([datasettype,value]) => ({datasettype,value}));
-}) }
-else {
-  console.log("hmmm")
-}
-}
-
-  function loadDatabase() {
-    return  fetch(neotomaapi + "/v2.0/data/dbtables/constituentdatabases?count=false&limit=5000&offset=0",
-      { method: "GET", headers: {'content-type': 'application/json'}})
-    .then(res1 => {
-      return res1.json()})
-    .then(json1 => {
-      databasekeys.value = json1.data  
-      currentDB.value = databasekeys.value.find(element => element.databaseid === Number(route.params.databaseid))
-         
-      databasename.value = currentDB.value.databasename
-      return databasename.value})
-    .then(val => fetch(neotomaapi + "/v2.0/data/datasets/db?limit=10000&offset=0&database=" +val,
-      { method: "GET", headers: {'content-type': 'application/json'}}))
-    .then(res => {
-      if (!res.ok) {
-        const error = new Error(res.statusText)
-        error.json = {'error': res.json(), 'databaseid':route.params.databaseid}
-        throw error;}
-      return res.json()})
-    .then(json => {
-      databaseinfo.value = json.data
-      uniqueSites.value = new Set();
-      databaseinfo.value.forEach(obj => uniqueSites.value.add(obj.site['siteid']));
-      uniqueSites.value = uniqueSites.value.size
-
-
-      datasetids.value = databaseinfo.value.flatMap(site =>
-          site.site.datasets.flatMap(dataset => dataset.datasetid))
-
-      datasets.value = databaseinfo.value.reduce((acc, obj) => {
-        obj.site.datasets.forEach(dataset => {
-          const type = dataset.datasettype;
-          acc[type] = (acc[type] || 0) + 1;})
-          return acc;}, {});
-      pis.value = databaseinfo.value.reduce((acc, obj) => {
-        obj.site.datasets.forEach(dataset => {
-          dataset.datasetpi.forEach(pi => {
-            const type = pi.contactname;
-            acc[type] = (acc[type] || 0) + 1;}) })
-        return acc;}, {});
-
-      datasets_array.value = Object.entries(datasets.value).map(([datasettype,value]) => ({datasettype,value}));
-      pis_array.value = Object.entries(pis.value).map(([name,value]) => ({name,value}));
-      pis_array.value = pis_array.value.filter(obj => obj.name !== 'null')
-      loading.value = false
-
-      return databaseinfo.value})
-}
-
-function loadContact() {
-  return  fetch(neotomaapi + "/v2.0/data/dbtables/constituentdatabases?count=false&limit=5000&offset=0",
-  { method: "GET", headers: {'content-type': 'application/json'}})
-  .then(res1 => {
-    return res1.json()})
-  .then(json1 => {
-    databasekeys.value = json1.data  
-    currentDB.value = databasekeys.value.find(element => element.databaseid === Number(route.params.databaseid))
-    contact.value = currentDB.value.contactid
-    return contact.value})
-  .then(val => fetch(neotomaapi + "/v1.5/data/contacts/" +val,
-  { method: "GET", headers: {'content-type': 'application/json'}}))
-  .then(res => {
-    if (!res.ok) {
-      const error = new Error(res.statusText)
-      error.json = {'error': res.json(), 'databaseid':route.params.databaseid}
-      throw error;}
-    return res.json()})
-  .then(json => {
-    contactinfo.value = json.data      })}
-
-    
 onMounted(() => {
 
  // loadDatabase()
-  loadContact()
-  loadconstDB();
+ // loadContact()
+ // loadconstDB();
   
 })
 
@@ -160,14 +58,12 @@ onMounted(() => {
       <template #header>
         <h2>Spatial and Temporal Extent</h2>
       </template>
-      <TabView>
-        <TabPanel header="Spatial Extent">
+      <h3>Spatial Extent</h3>
         <DatabaseMap />
-      </TabPanel>
-      <TabPanel header="Temporal Extent">
+
+      <h3>Temporal Extent</h3>
           <TempExtent/>
-      </TabPanel>
-    </TabView>
+
     </Panel>
       <UploadsTime/>
 
