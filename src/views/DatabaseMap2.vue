59c68685e776c39360a931ab9d673202a06ac37d
hoffmanick <134728053+hoffmanick@users.noreply.github.com>
2024-04-14 18:55:55 -0700

    clustered map
    I added a new map component that clusters sites if they're close enough together. Right now I have both maps (clustered and unclustered) in database.vue. The clustered map does seem to be a bit faster but I still prefer the unclustered map because it's not that slow and it gives you a better sense of where things really are

---
+++ src/views/DatabaseMap2.vue
@@ -0,0 +1,292 @@
+<script setup>
+import { Map, View } from 'ol';
+import { ref,onMounted} from 'vue';
+import {useRoute} from 'vue-router'
+import ProgressSpinner from 'primevue/progressspinner';
+import Dialog from 'primevue/dialog';
+import XYZ from 'ol/source/XYZ';
+import OSM from 'ol/source/OSM';
+import {boundingExtent} from 'ol/extent.js';
+import Cluster from 'ol/source/Cluster';
+import Tile from 'ol/layer/Tile.js';
+import VectorSource from 'ol/source/Vector';
+import VectorLayer from 'ol/layer/vector';
+import {fromLonLat} from 'ol/proj';
+import Point from 'ol/geom/Point';
+import Feature from "ol/Feature";
+import {Style} from 'ol/style';
+import Stroke from 'ol/style/Stroke';
+import Circle from 'ol/style/Circle';
+import Fill from 'ol/style/Fill';
+import Text from 'ol/style/Text';
+const route = useRoute()
+const datasets = ref([]);
+var names = ref([]);
+var ids = ref([]);
+var descrips = ref([]);
+var uniquenames = ref([]);
+var uniqueids = ref([]);
+var link = ref([]);
+const myMap = ref(null);
+const loading2 = ref(true)
+
+
+const vectorSource = new VectorSource();
+
+
+const visible = ref(false);
+const constDBinfo = ref(null);
+const neotomaapi = import.meta.env.VITE_APP_API_ENDPOINT ?? 'https://api.neotomadb.org'
+
+
+
+
+const vectorStyle = new Style({
+  image: new Circle({
+    radius: 12,
+    fill: new Fill({
+      color: 'rgba(251,205,188,1)',  }),
+    stroke: new Stroke({
+      color: 'rgba(92,84,80,1)',
+      width: 6,}), }),});
+            
+    function loadconstDB() {
+    return  fetch(neotomaapi + "/v2.0/apps/constdb/datasets?dbid=" + route.params.databaseid,
+      { method: "GET", headers: {'content-type': 'application/json'}})
+    .then(res1 => {
+      return res1.json()})
+    .then(json1 => {
+      constDBinfo.value = json1.data
+  return constDBinfo.value})
+  .then(values => {
+    let ids = [];
+        values.forEach((pointfull) => {
+    if (pointfull.coords !== null) { 
+      if (!ids.includes(pointfull.siteid)) {
+            var point = pointfull.coords
+            const lonLat = fromLonLat(point);
+            var feature = new Feature({
+                geometry: new Point(lonLat),
+                name: pointfull.sitename,
+                id: pointfull.siteid,
+        })
+          ids.push(pointfull.siteid)
+                
+            vectorSource.addFeature(feature);
+
+    }}
+})
+
+return vectorSource;})
+.then(source => {
+
+const clusterSource = new Cluster({
+  distance: 50,
+  minDistance: 50,
+  source:source});
+
+  const styleCache = {};
+
+  const clusters = new VectorLayer ({
+    source:clusterSource,
+    style: function (feature) {
+      const size = feature.get('features').length;
+      let style = styleCache[size];
+      if (!style) {
+        if (size==1) {
+          style = new Style({
+  image: new Circle({
+    radius: (5/4*4+1),
+    fill: new Fill({
+      color: 'rgba(251,205,188,1)',  }),
+    stroke: new Stroke({
+      color: 'rgba(92,84,80,1)',
+      width: (2/3*4),}), }),})
+        }
+        else {
+        style = new Style({
+          image: new Circle({
+            radius: size**(1/2.35)+5,
+            stroke: new Stroke({
+              color: 'rgb(60,50,40)',
+            }),
+            fill: new Fill({
+              color: 'rgb(221,205,188)',
+            }),
+          }),
+          text: new Text({
+            text: size.toString(),
+            fill: new Fill({
+              color: 'rgb(60,50,40)',
+            }),
+            font: 'bold ' + (size**(1/4.35) + 8) + 'px Helvetica',
+          }),
+        })};
+        styleCache[size] = style;
+      }
+      return style;
+    },
+  })
+
+return clusters})
+.then(vl => {
+  myMap.value = new Map({
+    layers: [
+        new Tile({
+            source: new XYZ({
+            url: 'https://tiles.stadiamaps.com/tiles/stamen_terrain/{z}/{x}/{y}.png'
+        }),}),
+        vl,],
+    target: 'map2',
+    view: new View({
+        center: [0,0],
+        zoom: 2,}),
+})
+
+
+myMap.value.on('moveend', function() {
+    var zoom = myMap.value.getView().getZoom()
+    console.log(zoom)
+    const newStyle = new Style({
+  image: new Circle({
+    radius: (5/4*zoom+1),
+    fill: new Fill({
+      color: 'rgba(251,205,188,1)',  }),
+    stroke: new Stroke({
+      color: 'rgba(92,84,80,1)',
+      width: (2/3*zoom),}), }),});;
+
+    //  vl.setStyle(newStyle);
+
+
+    });
+
+myMap.value.on('click', (e) => {
+  vl.getFeatures(e.pixel).then((clickedFeatures) => {
+    if (clickedFeatures.length) {
+      // Get clustered Coordinates
+      const features = clickedFeatures[0].get('features');
+      if (features.length > 1) {
+        const extent = boundingExtent(
+          features.map((r) => r.getGeometry().getCoordinates()),
+        );
+        myMap.value.getView().fit(extent, {duration: 1000, padding: [50, 50, 50, 50]});
+      }
+    }
+  });
+});
+  
+})
+}
+
+onMounted(async () => {
+    await loadconstDB();
+    var extent2 = vectorSource.getExtent()
+    //console.log(((extent2[2]-extent2[0])+(extent2[3]-extent2[1]))/2)
+    var padding = ((extent2[2]-extent2[0])+(extent2[3]-extent2[1]))/20
+    var paddedExtent = [extent2[0] - padding, extent2[1] - padding,
+  extent2[2] + padding, extent2[3] + padding];
+    myMap.value.getView().fit(paddedExtent);
+ 
+var displayFeatureInfo = function(pixel, coordinate) {
+    var features2 = [];
+    names.value = [];
+    ids.value = [];
+    uniquenames.value = [];
+    uniqueids.value = [];
+    link.value = [];
+    datasets.value = [];
+    myMap.value.forEachFeatureAtPixel(pixel, function(feature, layer) {
+        console.log(feature);
+        feature.values_.features.forEach(el => {features2.push(el); });});
+    
+    if (features2.length == 1) {
+      console.log('true')
+
+        for (var i = 0, ii = features2.length; i < ii; ++i) {
+            names.value.push(features2[i].get('name'));
+            ids.value.push(features2[i].get('id'));}
+        uniquenames.value = [... new Set(names.value.map(item => item))];
+        uniqueids.value = [... new Set(ids.value.map(item => item))];
+        if (uniquenames.value.length == 1) {
+          visible.value=true
+            var sets = fetch('https://api.neotomadb.org/v2.0/data/sites/' + uniqueids.value[0] + '/datasets')
+            .then(res =>{
+                var inter = res.json()
+                console.log(inter)
+            return inter
+
+            })
+            .then(int => {
+                var all = int.data
+                var fullsets = all.flatMap(site => site.site.datasets).map(on => on);
+                console.log(fullsets)
+                console.log(all)
+                fullsets.forEach(entry => {
+                    datasets.value.push(entry.datasetid)
+                    link.value.push(("https://data.neotomadb.org/datasets/" + entry.datasetid))
+                })
+                return datasets.value
+                
+            })
+        
+        }
+} 
+};
+
+myMap.value.on('click', async function(evt) {
+  console.log(await uniquenames.value)
+
+    var coordinate = evt.coordinate;
+    displayFeatureInfo(evt.pixel, coordinate); })
+
+
+
+loading2.value =false
+
+
+
+})
+
+
+</script>
+
+
+
+<style>
+.map {
+  width: 100%;
+  height: 400px; /* Adjust the height as needed */
+}
+
+</style>
+
+
+<template>
+<div>
+    <div style="width:750px;margin-left:auto;margin-right:auto;border:3px solid rgb(92,84,80);">
+      <div v-if="loading2" class="flex flex-wrap justify-content-center align-items-center">
+         <ProgressSpinner class="flex-grow-1 w-max" />
+       </div>
+      <div id='map2' class="map"> </div>
+    </div>  
+    <div v-if="uniquenames.length == 1">
+        <Dialog
+            v-model:visible="visible"
+            modal
+            :header="names[0]"
+            :style="{ width: '50rem' }"
+            :breakpoints="{ '1199px': '75vw', '575px': '90vw' }"
+        >
+            <p>Site ID: {{ ids[0] }}</p>
+            <p>Dataset ID links:</p>
+            <div v-for="(el,index) in link">
+                <a :href="link[index]" target="_blank">{{ datasets[index] }}</a>
+            </div>
+            <p>{{ descrips[0] }}</p>
+        </Dialog>
+    </div>
+</div>
+
+
+</template>
--- src/views/database.vue
+++ src/views/database.vue
@@ -1,6 +1,7 @@
 <script setup>
   import TempExtent from '@/views/TempExtent.vue';
   import DatabaseMap from '@/views/DatabaseMap.vue';
+  import DatabaseMap2 from '@/views/DatabaseMap2.vue';
   import UploadsTime from '@/views/UploadsTime.vue';
   import Panel from 'primevue/panel'
   import AboutDB from '@/views/AboutDB.vue';
@@ -17,6 +18,7 @@
       </template>
       <h3>Spatial Extent</h3>
         <DatabaseMap />
+        <DatabaseMap2 />
       <h3>Temporal Extent</h3>
           <TempExtent/>
     </Panel>
