caf6327201197a9fa593116bf44842b7d143fc07
Simon Goring <simon.j.goring@gmail.com>
2024-06-04 10:32:24 -0700

    Auto stash before checking out "HEAD"

--- src/views/testmap.vue
+++ src/views/testmap.vue
@@ -1,7 +1,7 @@
 <script setup>
  import { ref, onMounted } from 'vue';
  import { useRoute } from 'vue-router';
- import DatabaseMap from '@/views/DatabaseMap.vue';
+ import DatabaseMap from '@/views/resources/databaseelements/DatabaseMap2.vue';
 
  
   const route = useRoute()
