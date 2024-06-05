aa88d71b7a9d459a718c45e5d8aa33421cb9bbeb
Simon Goring <simon.j.goring@gmail.com>
2024-06-04 18:06:56 -0700

    Updated the package & cleaned variables/api calls
    Working to make some of this run faster.
    Took the existing workflow and updated some of the fnctions/calls to work more closely with the Neotoma API.

--- src/views/resources/databaseelements/AboutDB.vue
+++ src/views/resources/databaseelements/AboutDB.vue
@@ -7,17 +7,17 @@ import { ref, onMounted } from 'vue'
 import {useRoute} from 'vue-router'
 import ProgressSpinner from 'primevue/progressspinner'
 
-const contact = ref(0)
-const contactinfo = ref(0)
+const contact = ref(null);
+const email = ref(null);
+const databaseurl = ref(null);
 const uniqueSites = ref(null);
 const loading_ab = ref(true);
 const databasekeys = ref(null)
-const databasename = ref(null)
+const databasename = ref("Constituent Database")
 const currentDB = ref(null)
 const route = useRoute();
-const constDBinfo = ref(null);
 const uniqueDBsites = ref(null);
-const uniqueDBsets = ref(null);
+const uniqueDBdatasets = ref(null);
 const datasettypes = ref(null);
 const textDB = ref(null);
 const htmlString = ref(null);
@@ -26,64 +26,35 @@ const neotomaapi = import.meta.env.VITE_APP_API_ENDPOINT ?? 'https://api.neotoma
 
 uniqueSites.value = new Set();
 
-function loadContact() {
-  return  fetch(neotomaapi + "/v2.0/data/dbtables/constituentdatabases?count=false&limit=5000&offset=0",
-  { method: "GET", headers: {'content-type': 'application/json'}})
-  .then(res1 => {
-    return res1.json()})
-  .then(json1 => {
-    databasekeys.value = json1.data  
-    currentDB.value = databasekeys.value.find(element => element.databaseid === Number(route.params.databaseid))
-    contact.value = currentDB.value.contactid
-    databasename.value = currentDB.value.databasename
-    return contact.value})
-  .then(val => fetch(neotomaapi + "/v1.5/data/contacts/" + val,
-  { method: "GET", headers: {'content-type': 'application/json'}}))
-  .then(res => {
-    if (!res.ok) {
-      const error = new Error(res.statusText)
-      error.json = {'error': res.json(), 'databaseid':route.params.databaseid}
-      throw error;}
-    return res.json()})
-  .then(json => {
-    contactinfo.value = json.data      })}
-
-
-function loadconstDB() {
-    return  fetch(neotomaapi + "/v2.0/apps/constdb/datasets?dbid=" + route.params.databaseid,
-      { method: "GET", headers: {'content-type': 'application/json'}})
+function loadConstDB() {
+  return  fetch(neotomaapi + "/v2.0/apps/constdb?dbid=" + route.params.databaseid,
+    { method: "GET",
+    headers: {'content-type': 'application/json'}})
     .then(res1 => {
-      return res1.json()})
+      console.log(res1)
+      return res1.json()
+    })
     .then(json1 => {
-      constDBinfo.value = json1.data
+      databasekeys.value = json1.data[0]
       
-   
-      uniqueDBsites.value = new Set();
-      constDBinfo.value.forEach(obj => uniqueDBsites.value.add(obj['siteid']));
-      uniqueDBsites.value = uniqueDBsites.value.size
-
-      uniqueDBsets.value = new Set();
-      constDBinfo.value.forEach(obj => uniqueDBsets.value.add(obj['datasetid']));
-      uniqueDBsets.value = uniqueDBsets.value.size
-      datasettypes.value = constDBinfo.value.reduce((acc, obj) => {
-          const type = obj.datasettype;
-          acc[type] = (acc[type] || 0) + 1;
-          return acc;}, {});
-
-  datasettypes.value = Object.entries(datasettypes.value).map(([datasettype,value]) => ({datasettype,value}));
-  loading_ab.value=false
-})
+      currentDB.value = route.params.databaseid
+      contact.value = databasekeys.value.database['contact']
+      email.value = databasekeys.value.database['email'] || 'no email provided'
+      databaseurl.value = databasekeys.value.database['url'] || 'no URL provided'
+      databasename.value = databasekeys.value.database['databasename']
+      uniqueDBsites.value = databasekeys.value.sitecount
+      datasettypes.value = databasekeys.value.datasettypes
+      loading_ab.value = false
+    })
 }
 
-
 onMounted(() => {
-    loadContact()
-    loadconstDB()
+    loadConstDB()
     if (textobj.filter(a => a.dbID == route.params.databaseid).length != 0) {
       let texttry = textobj.filter(a => a.dbID == route.params.databaseid)[0].dbDescrip
       textDB.value = texttry}
       else {
-        textDB.value = ""
+        textDB.value = "There is no description provided for this constituent database. "
       }
       htmlString.value = textDB.value
     
@@ -96,24 +67,23 @@ onMounted(() => {
 <template>
   <Panel toggleable>
     <template #header>
-      <h2>About {{ databasename }}</h2>
+      <h2>About the {{ databasename }}</h2>
     </template>
-    <div v-if="!loading_ab"> 
+    <div v-if="!loading_ab">
+    <h3>Constituent Database Description</h3>
       <div v-html="htmlString"></div>
-      <p>There are {{uniqueDBsites}} distinct sites in {{ databasename }}, associated with {{ uniqueDBsets }} datasets.</p>
+      
+    <h3>Constituent Database Metadata</h3>
+    <ul>
+      <li>Database Contact : {{ contact }} [{{email}}]</li>
+      <li>Database URL : {{ databaseurl }}</li>
+    </ul>
+    <h3>Sites and Datasets</h3>      
+      <p>There are {{uniqueDBsites}} distinct sites in {{ databasename }}.</p>
       <DataTable paginator :rows="5" :value="datasettypes" :sort-field="'value'" :sort-order="-1" tableStyle="min-width: 20rem">
         <Column field="datasettype" header="Dataset Type"></Column>
-        <Column field="value" header="Number" sortable></Column>
+        <Column field="datasets" header="Number of Datasets" sortable></Column>
       </DataTable>
-        
-      <div v-for="(value, name, index) in contactinfo" :key="name.id">
-        <div v-if="value.url !== null">
-          <p> <span> <strong> Database Contact</strong>: <a :href='value.url'> {{ value.givennames}} {{ value.familyname }} </a> </span></p>
-        </div>
-        <div v-else>
-            <p> <span> <strong> Database Contact</strong>: <p> {{ value.givennames}} {{ value.familyname }} </p> </span></p>
-        </div>
-      </div>
     </div>
     <div v-else class="flex flex-wrap justify-content-center align-items-center">
       <ProgressSpinner class="flex-grow-1 w-max" />
