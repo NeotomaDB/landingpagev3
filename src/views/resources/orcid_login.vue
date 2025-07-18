6f50a10ac3e0058d71bef0e355cdb60964adf38e
Simon <simon.j.goring@gmail.com>
2025-07-18 12:26:55 -0700

    Minor fix.

--- src/views/resources/orcid_login.vue
+++ src/views/resources/orcid_login.vue
@@ -6,6 +6,7 @@ import { useRoute } from 'vue-router';
 
 const route = useRoute();
 
+const apiLocation = import.meta.env.VITE_APP_API_URL || 'https://api.neotomadb.org';
 const baseUrl = import.meta.env.VITE_APP_ORCID_LOGIN || "https://orcid.org/oauth/authorize?response_type=token&redirect_uri=https://data.neotomadb.org/login&client_id=APP-OKAEGWFY7MEOK4HE&scope=openid";
 const userValidation = import.meta.env.VITE_APP_API_URL + '/v2.0/apps/orcids/validate';
 
