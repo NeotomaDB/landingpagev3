8a04010058c449c9180550f58a131c87ee731343
Simon <simon.j.goring@gmail.com>
2025-09-10 12:16:38 -0700

    Fixing more filenames.
    Also adding a 'functions' folder, where we can start putting js functions so that they're more organized.

--- src/views/UserPage.vue
+++ src/views/UserPage.vue
@@ -1,40 +1,40 @@
 <script setup>
-  import { onMounted, ref } from 'vue'
-  import Panel from 'primevue/panel'
-  import Card  from 'primevue/card'
-  import { useRoute, useRouter } from 'vue-router'
-  import ContactDetails from '@/views/resources/contactelements/contactdetails.vue'
-  import ContactPublications from '@/views/resources/contactelements/contactlinkedpublications.vue'
-  import ContactDatasets from '@/views/resources/contactelements/contactlinkeddatasets.vue'
+import { onMounted, ref } from 'vue'
+import Panel from 'primevue/panel'
+import Card from 'primevue/card'
+import { useRoute, useRouter } from 'vue-router'
+import ContactDetails from '@/views/resources/contactelements/ContactDetails.vue'
+import ContactPublications from '@/views/resources/contactelements/ContactLinkedPublications.vue'
+import ContactDatasets from '@/views/resources/contactelements/ContactLinkedDatasets.vue'
 
-  const route = useRoute();
-  const router = useRouter();
+const route = useRoute();
+const router = useRouter();
 
-  const neotomaapi = import.meta.env.VITE_APP_API_ENDPOINT
-  let loading = ref(true)
-  let contactinfo = ref(null)
+const neotomaapi = import.meta.env.VITE_APP_API_ENDPOINT
+let loading = ref(true)
+let contactinfo = ref(null)
 
-  // Initialize on mount
-  onMounted(async () => {
-    await router.isReady()
-    contactinfo.value = route.params.contactid;
-    if (!contactinfo.value) {
-      const userValidation = localStorage.getItem("orcid_user");
-      if (userValidation) {
-        const response = await fetch(neotomaapi + `/v2.0/apps/landing/orcids/orcid?{userOrcid}`, {
-          method: 'GET',
-        }
-        )
-        if (!response.ok) {
-          loading.value = false
-          console.log('Issue with the API call.')
-          throw new Error(`Response status: ${response.status}`);
-        }
-        const json = await response.json();
-        contactinfo.value = json['data']
+// Initialize on mount
+onMounted(async () => {
+  await router.isReady()
+  contactinfo.value = route.params.contactid;
+  if (!contactinfo.value) {
+    const userValidation = localStorage.getItem("orcid_user");
+    if (userValidation) {
+      const response = await fetch(neotomaapi + `/v2.0/apps/landing/orcids/orcid?{userOrcid}`, {
+        method: 'GET',
       }
+      )
+      if (!response.ok) {
+        loading.value = false
+        console.log('Issue with the API call.')
+        throw new Error(`Response status: ${response.status}`);
+      }
+      const json = await response.json();
+      contactinfo.value = json['data']
     }
-  });
+  }
+});
 
 </script>
 
@@ -46,12 +46,18 @@
   </div>
   <div else>
     <Panel>
-      <template #header><h1>Neotoma Contacts</h1></template>
-      <p>This page is intended as a landing page for Neotoma contacts. This includes authors, stewards, data contributors, analysts and others.</p>
+      <template #header>
+        <h1>Neotoma Contacts</h1>
+      </template>
+      <p>This page is intended as a landing page for Neotoma contacts. This includes authors, stewards, data
+        contributors,
+        analysts and others.</p>
 
-      <p>Neotoma is the product of thousands of contributors working as sample analysts, sample collectors, authors, data modellers and stewards, among other activities.</p>
+      <p>Neotoma is the product of thousands of contributors working as sample analysts, sample collectors, authors,
+        data modellers and stewards, among other activities.</p>
 
-      <p>Individual contributions are difficult to encapsulate into simple statistics, but we hope that these summaries can help showcase the work individuals have put into the database.</p>
+      <p>Individual contributions are difficult to encapsulate into simple statistics, but we hope that these summaries
+        can help showcase the work individuals have put into the database.</p>
       <Card>
         <template #content>
           User Search coming soon.
@@ -63,10 +69,10 @@
 </template>
 
 <script>
-  export default {
-    name: 'UserPage',
-    data () {
-      return {}
-      }
-    }
+export default {
+  name: 'UserPage',
+  data() {
+    return {}
+  }
+}
 </script>
